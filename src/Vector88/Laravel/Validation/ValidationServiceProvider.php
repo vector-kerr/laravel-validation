532b12868bedde1e86c81de349914bb3ab9ef765
Daniel Kerr <vector.kerr@gmail.com>
2016-08-17 20:19:08 +1000

    Added missing 'new' keyword in validation service provider

--- src/Vector88/Laravel/Validation/ValidationServiceProvider.php
+++ src/Vector88/Laravel/Validation/ValidationServiceProvider.php
@@ -9,7 +9,7 @@ class ValidationServiceProvider extends ServiceProvider {
 	public function register()
 	{	
 		$this->app->bind( 'LaravelValidation', function() { return new LaravelValidationService(); } );
-		$this->app->bind( 'AngularValidation', function() { return AngularValidationService(); } );
+		$this->app->bind( 'AngularValidation', function() { return new AngularValidationService(); } );
 		$this->app->tag( [ 'LaravelValidation', 'AngularValidation' ], 'ValidationServices' );
 		$this->app->bind( 'Validation', function( $app ) { return new ValidationService( $app, $app->tagged( 'ValidationServices' ) ); } );
 	}
