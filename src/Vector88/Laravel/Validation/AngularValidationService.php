4b60597e325b73fefc2a608d6da7ffbb3eec93a3
Daniel Kerr <vector.kerr@gmail.com>
2016-08-17 20:12:30 +1000

    implemented contract in angular and laravel validation services

--- src/Vector88/Laravel/Validation/AngularValidationService.php
+++ src/Vector88/Laravel/Validation/AngularValidationService.php
@@ -4,6 +4,13 @@ namespace Vector88\Laravel\Validation;
 
 class AngularValidationService implements ValidationServiceContract {
 	
-	
+	public function getKey() {
+		return 'angular';
+	}
+    
+	public function setData( $data ) {
+		throw new Exception( "Not Implemented" );
+		return $this;
+	}
 	
 }
--- src/Vector88/Laravel/Validation/LaravelValidationService.php
+++ src/Vector88/Laravel/Validation/LaravelValidationService.php
@@ -4,6 +4,13 @@ namespace Vector88\Laravel\Validation;
 
 class LaravelValidationService implements ValidationServiceContract {
 	
-	
+	public function getKey() {
+		return 'laravel';
+	}
+    
+	public function setData( $data ) {
+		throw new Exception( "Not Implemented" );
+		return $this;
+	}
 	
 }
