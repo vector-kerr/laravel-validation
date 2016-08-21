f9a67f84d28e33afb201a56f28e6fa9bb0026c02
Daniel Kerr <vector.kerr@gmail.com>
2016-08-21 15:28:27 +1000

    Added validation error messages to angular validation service

--- src/Vector88/Laravel/Validation/AngularValidationService.php
+++ src/Vector88/Laravel/Validation/AngularValidationService.php
@@ -8,6 +8,14 @@ class AngularValidationService implements ValidationServiceContract {
 	protected $currentFieldRules;
 	protected $currentFieldAttributes;
 	
+	protected $messages = [
+		'required' => 'The {{field}} field is required.',
+		'string' => 'The {{field}} field must be a string.',	// TODO: use more human-understandable term than 'string'.
+		'min' => 'The {{field}} field has a minimum length of {{value}} characters.',
+		'max' => 'The {{field}} field has a maximum length of {{value}} characters.',
+		'regex' => 'The {{field}} field doesn\'t look right.',	// TODO: surely, brain, you can do better than this.
+	];
+	
 	public function getKey() {
 		return 'angular';
 	}
@@ -64,6 +72,49 @@ class AngularValidationService implements ValidationServiceContract {
 		$result = preg_replace( $expr, $value, $attributeValue );
 		$this->currentFieldAttributes[ $attributeKey ] = $result;
 		return $this;
+	}
+	
+	public function allMessages() {
+		$messages = [];
+		foreach( $this->data as $field => $unusedValue ) {
+			$messages[ $field ] = $this->messages( $field );
+		}
+		return $messages;
+	}
+	
+	public function messages( $field ) {
+		$rules = $this->data[ $field ];
+		$result = [];
+		
+		foreach( $rules as $rule => $ruleValues ) {
+			if( array_key_exists( $rule, $this->messages ) ) {
+				$ruleData = [ 'field' => $field ];
+				$ruleData = array_merge( $ruleData, ( is_array( $ruleValues ) ? $ruleValues : [ 'value' => $ruleValues ] ) );
+				$keys = array_map( function( $x ) { return "/\{\{{$x}\}\}/i"; }, array_keys( $ruleData ) );
+				$subs = array_values( $ruleData );
+				$message = $this->messages[ $rule ];
+				$result[ $rule ] = preg_replace( $keys, $subs, $message );
+			}
+		}
+		
+		return $result;
+	}
+	
+	public function allMessageElements() {
+		$messageElements = [];
+		foreach( $this->data as $field => $unusedValue ) {
+			$messageElements[ $field ] = $this->messageElements( $field );
+		}
+		return $messageElements;
+	}
+	
+	public function messageElements( $field ) {
+		$messages = $this->messages( $field );
+		$parts = [];
+		foreach( $messages as $rule => $message ) {
+			$parts[] = "<div ng-message=\"{$rule}\">{$message}</div>";
+		}
+		return $parts;
 	}
 	
 }
